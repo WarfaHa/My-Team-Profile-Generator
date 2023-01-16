c60d8701cac44e44700c8ff65b35379cdd878946
Warfa <warfahassan35@yahoo.com>
2023-01-15 21:55:05 -0500

    finished user input of the team questions

--- index.js
+++ index.js
@@ -5,49 +5,146 @@ const inquirer = require("inquirer");
 // Opening Statement/Introduction
 console.log("Let's generate a webpage that displays your team's basic info!");
 
+
 // An array of questions for user input
-const questions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What is the team manager’s name:",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What is the team manager’s employee ID:",
-  },
-  {
-    type: "input",
-    name: "managerEmail",
-    message: "What is the team manager’s email address:",
-  },
-  {
-    type: "input",
-    name: "officeNumber",
-    message: "What is the team manager’s office number:",
-  },
-  {
+const teamArray = []; 
+
+// This function initializes app to prompt user to add an employee
+const addEmployee = () => {
+  inquirer.prompt([{
     type: "list",
-    name: "options",
-    choices: [
-        "add an engineer",
-        "add an intern", 
-        "finish building your team",
-    ],
-  },
-];
-// team manager’s name, employee ID, email address, and office number
+    message: "What type of employee would you like to add to your team?",
+    name: "addEmployeeOption",
+    choices: ["Manager", "Engineer", "Intern", "finish building my team"]
+  }]).then(function (userInput) {
+    switch(userInput.addEmployeeOption) {
+      case "Manager":
+        addManager();
+        break;
+      case "Engineer":
+        addEngineer();
+        break;
+      case "Intern":
+        addIntern();
+        break;
+
+        default:
+          writeToFile();
+}
+})
+
+}
+
+function addManager(){
+    inquirer.prompt ([
+      
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is the manager's name?"
+      },
+  
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is the manager's employee ID number?"
+      },
+  
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the manager's email address?"
+      },
+  
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?"
+      }
+      
+    ]).then(function (userInput) {
+
+      //testing user input
+      console.log(userInput);
+      addEmployee();
+    });
+
+}
+function addEngineer(){
+  
+  inquirer.prompt ([
+      
+    {
+      type: "input",
+      name: "engineerName",
+      message: "What is the engineer's name?"
+    },
+
+    {
+      type: "input",
+      name: "engineerId",
+      message: "What is the engineer's ID number?"
+    },
+
+    {
+      type: "input",
+      name: "engineerEmail",
+      message: "What is the engineer's email address?"
+    },
+
+    {
+      type: "input",
+      name: "engineerGithub",
+      message: "What is the engineer's GitHub username?"
+    }
+    
+  ]).then(function (userInput) {
+
+    //testing user input
+    console.log(userInput);
+    addEmployee();
+  });
+
+}
+function addIntern(){
+  inquirer.prompt ([
+      
+    {
+      type: "input",
+      name: "internName",
+      message: "What is the intern's name?"
+    },
+
+    {
+      type: "input",
+      name: "internId",
+      message: "What is the intern's ID number?"
+    },
+
+    {
+      type: "input",
+      name: "internEmail",
+      message: "What is the intern's email address?"
+    },
+
+    {
+      type: "input",
+      name: "internSchool",
+      message: "What school did the intern go to?"
+    }
+    
+  ]).then(function (userInput) {
+
+    //testing user input
+    console.log(userInput);
+    addEmployee();
+  });
+};
+
 // This function writes a HTML file
 function writeToFile() {}
 
-// This function initializes app
-function init() {
-  return inquirer.prompt(questions).then(function (userInput) {
-    //   Making sure input is correct
-    console.log(userInput);
-  });
-}
-
 // Function call to initialize app
-init();
+addEmployee();
+
+
